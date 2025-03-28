c71d6cb3f47f91a8772979ba3c33d70a865d530e
marcoos-rg <rosadogonzalezmarcos2@gmail.com>
2025-03-28 11:15:54 +0100

    Primer commit

---
+++ src/main/java/es/upm/dit/isst/flores/FloresApplication.java
@@ -0,0 +1,13 @@
+package es.upm.dit.isst.flores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FloresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FloresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/upm/dit/isst/flores/FloresApplicationTests.java
@@ -0,0 +1,13 @@
+package es.upm.dit.isst.flores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FloresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
