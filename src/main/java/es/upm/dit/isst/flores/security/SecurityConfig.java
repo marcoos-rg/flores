be360c794353f18fafba4a4792a4e4020146e01e
aanunnezz <antonio.nunez.desantos@alumnos.upm.es>
2025-03-29 14:00:46 +0100

    Actualización para añadir el puerto 3001

--- src/main/java/es/upm/dit/isst/flores/security/SecurityConfig.java
+++ src/main/java/es/upm/dit/isst/flores/security/SecurityConfig.java
@@ -19,7 +19,7 @@ public class SecurityConfig {
             .csrf().disable()
             .authorizeHttpRequests(authz -> authz.anyRequest().permitAll());
 
-        return http.build();
+        return http.build();    
     }
 
     // Configuración CORS general (como ya tenías)
@@ -29,7 +29,7 @@ public class SecurityConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins("http://localhost:3001, http://localhost:3000")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
             }
